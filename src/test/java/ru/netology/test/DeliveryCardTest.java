fe56600a9edf08246fff4a686fbe26cd245f468b
Aleksandr Osipov <eltisprim@mail.ru>
2021-06-18 16:01:44 -0400

    Created new tests

--- src/test/java/ru/netology/test/DeliveryCardTest.java
+++ src/test/java/ru/netology/test/DeliveryCardTest.java
@@ -49,4 +49,79 @@ public class DeliveryCardTest {
 
     }
 
+    @Test
+    void shouldEnterEmptyCity() {
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.forwardDate(3));
+        $("[data-test-id='name'] input").setValue(registrationInfo.getFullName());
+        $("[data-test-id='phone'] input").setValue(registrationInfo.getPhoneNumber());
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $(".input_invalid[data-test-id='city']").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldEnterNotValidCity() {
+        $("[data-test-id='city'] input").setValue("Wendell");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.forwardDate(3));
+        $("[data-test-id='name'] input").setValue(registrationInfo.getFullName());
+        $("[data-test-id='phone'] input").setValue(registrationInfo.getPhoneNumber());
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $(".input_invalid[data-test-id='city']").shouldHave(text("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldEnterNotValidName() {
+        $("[data-test-id='city'] input").setValue("Воронеж");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.forwardDate(3));
+        $("[data-test-id='name'] input").setValue("Aleksandr Osipov");
+        $("[data-test-id='phone'] input").setValue(registrationInfo.getPhoneNumber());
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldEnterNotValidPhone() {
+        $("[data-test-id='city'] input").setValue("Воронеж");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.forwardDate(3));
+        $("[data-test-id='name'] input").setValue(registrationInfo.getFullName());
+        $("[data-test-id='phone'] input").setValue("+798012345");
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldEnterEmptyPhone() {
+        $("[data-test-id='city'] input").setValue("Воронеж");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.forwardDate(3));
+        $("[data-test-id='name'] input").setValue(registrationInfo.getFullName());
+        $("[data-test-id='phone'] input").setValue("");
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldEmptyCheckBox() {
+        $("[data-test-id='city'] input").setValue("Воронеж");
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.forwardDate(3));
+        $("[data-test-id='name'] input").setValue(registrationInfo.getFullName());
+        $("[data-test-id='phone'] input").setValue(registrationInfo.getPhoneNumber());
+        $(byText("Запланировать")).click();
+        $(".input_invalid[data-test-id='agreement']").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
 }
